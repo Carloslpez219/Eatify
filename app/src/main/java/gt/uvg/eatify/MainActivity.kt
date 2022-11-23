77bf9904ce24e2102a51a17d4155c625da46b1b5
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2022-11-22 20:23:40 -0600

    Implementacion modelo y repository

--- app/src/main/java/gt/uvg/eatify/MainActivity.kt
+++ app/src/main/java/gt/uvg/eatify/MainActivity.kt
@@ -1,11 +1,7 @@
 package gt.uvg.eatify
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import androidx.cardview.widget.CardView
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import gt.uvg.eatify.R
@@ -15,22 +11,21 @@ import androidx.navigation.ui.setupActionBarWithNavController
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-    var card: CardView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        card = findViewById<View>(R.id.cardView2) as CardView?
-        card?.setOnClickListener(
-            object : View.OnClickListener {
-                override fun onClick(v: View?) {
-                    val i = Intent(this@MainActivity, Detalle::class.java)
-                    startActivity(i)
-                }
-            }
-        )
+        // Inicializar data.
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host) as NavHostFragment
 
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
